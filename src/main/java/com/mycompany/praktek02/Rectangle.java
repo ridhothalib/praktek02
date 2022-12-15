7d138ed4df257ad4bc2897ed8a7cc1cf81b6a504
ridhothalib <108752148+ridhothalib@users.noreply.github.com>
2022-12-15 23:42:46 +0700

    Method dengan nilai balik

--- src/main/java/com/mycompany/praktek02/Rectangle.java
+++ src/main/java/com/mycompany/praktek02/Rectangle.java
@@ -3,10 +3,16 @@ public class Rectangle {
     double panjang;
     double lebar;
     
-    cetakInfo(){
+    void cetakInfo(){
         System.out.println("================");
         System.out.println("Panjang :"+panjang);
         System.out.println("Lebar :  "+lebar);
         System.out.println("================");
+    }
+    
+    double hitungLuas(){
+        double luas;
+        luas=panjang*lebar;
+        return luas;
     }
 }
